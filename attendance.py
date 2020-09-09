d1b2cbf126d412270c372b773dc111e6f0357519
soumyavemuri <60005707+soumyavemuri@users.noreply.github.com>
2020-09-09 09:24:37 +0530

    Update attendance.py
    Replaced destination file to Attendance.csv

--- attendance.py
+++ attendance.py
@@ -12,10 +12,10 @@ df.index.name = None
 left = df[df['User Action']=='Left']
 df = df[~df.index.duplicated(keep='first')]
 
-# - Replace 'C2.csv' with the name of your attendance log csv file
+# - Replace 'Attendance.csv' with the name of your attendance log csv file
 # - please ensure that the names of students exactly match the display name
 #   on Microsoft Teams
-df1 = pd.read_csv('V-SEM CSE-2 Attendance.csv', index_col=0)
+df1 = pd.read_csv('Attendance.csv', index_col=0)
 
 today = date.today().strftime("%d-%b-%Y")
 
@@ -29,7 +29,8 @@ for index in df.index:
     others.append(index)
 
 present = df1[df1[today]=='P']
-df1.to_csv('V-SEM CSE-2 Attendance.csv')
+# - Replace 'Attendance.csv' with the name of your attendance log csv file
+df1.to_csv('Attendance.csv')
 
 with open('Left.txt','w') as outfile:
     left.to_string(outfile)
